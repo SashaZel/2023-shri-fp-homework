10dddaffffc1af290272965327dffcf8e47c086a
Alexander Zelenkov <lll555@yandex.ru>
2023-07-23 22:30:52 +0700

    feat: add fp quirks

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -1,3 +1,5 @@
+import * as R from "ramda";
+
 /**
  * @file Домашка по FP ч. 2
  *
@@ -25,27 +27,146 @@
      setTimeout(resolve, time);
  })
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+//  const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
+//      /**
+//       * Я – пример, удали меня
+//       */
+//      writeLog(value);
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
+//      api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
+//          writeLog(result);
+//      });
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+//      wait(2500).then(() => {
+//          writeLog('SecondLog')
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
+//          return wait(1500);
+//      }).then(() => {
+//          writeLog('ThirdLog');
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+//          return wait(400);
+//      }).then(() => {
+//          handleSuccess('Done');
+//      });
+//  }
+
+console.clear();
+
+// Constants
+const justCopy = (a) => a;
+// Props
+const value = R.prop("value");
+const logFn= R.prop("writeLog");
+const successFn= R.prop("handleSuccess");
+const logError = R.prop("handleError");
+
+// 1: Log value
+const logValue = R.converge(R.call, [logFn, value])
+
+// 2a: validate input and set error
+// const validationErrorMessage = R.always("ValidationError");
+const stringLength = R.prop("length");
+const hasLengthLessThanTen = R.gt(10);
+const isLongerThanTen = R.compose(
+    R.not,
+    hasLengthLessThanTen,
+    stringLength,
+    value
+    );
+const hasLengthLongerThanTwo = R.lte(2); 
+const isShorterThanTwo = R.compose(
+    R.not,
+    hasLengthLongerThanTwo,
+    stringLength,
+    value
+    );
+const isDigitsAndDot = R.test(/^-?\d+\.?\d*$/);
+const isNotDigit = R.compose(
+    R.not,
+    isDigitsAndDot,
+    value
+);    
+const isNotValidString = R.anyPass([isShorterThanTwo, isLongerThanTen, isNotDigit]);
+const setError = R.assoc("error");
+const setValidationError = setError("ValidationError");
+const setEmptyError = setError("");
+const setErrorMessage = R.ifElse( isNotValidString, setValidationError, setEmptyError);
+
+// 3: Input string to number
+
+
+// 2b: Log Error
+const processHasError = R.prop("error");
+const loggingError = R.converge(R.call, [logError, processHasError])
+const logErrorIfExist = R.when(processHasError, loggingError);
+
+// Final: compose all
+const processSequence = R.compose(
+    R.tap(logErrorIfExist),
+    setErrorMessage,
+    R.tap(logValue)
+) 
 
 export default processSequence;
+////////////////////////////////////////////
+const petVal = R.prop("val");
+const petLogging = R.prop("logging");
+const petError = R.prop("error");
+const petString = "aaa";
+const petIsLongerThanTen = R.compose(
+    R.not,
+    hasLengthLessThanTen,
+    stringLength,
+    petVal
+);
+const petIsShorterThanTwo = R.compose(
+    R.not,
+    hasLengthLongerThanTwo,
+    stringLength,
+    petVal
+);
+
+const petIsValidString = R.anyPass([petIsShorterThanTwo, petIsLongerThanTen]);
+// const petLogWarning = R.converge(R.call, [petLogging, validationErrorMessage]);
+const petSetValidationError = R.ifElse( petIsValidString, setValidationError, setEmptyError);
+
+// console.log(hasLengthLessThanTen(8))
+// console.log(stringLength(petString))
+// console.log(isLongerThanTen(petString));
+
+// const petWriteLog = (logger, log) => logger(log);
+
+const is9 = R.test(/^-?\d+\.?\d*$/);
+
+// console.log(is9("9000.10"))
+//////  /////////  /////////  //////////  ////////// /  
+const petLogConverge = R.converge(R.call, [petLogging, petVal]);
+const petLogError = R.converge(R.call, [petLogging, petError])
+const petGetError = R.when(petError, petLogError);
+const petObj = {
+    val: "Joooo",
+    logging: console.log
+}
+const petLog = R.prop("logging");
+
+// const petFn = (args) => {
+//   args.logging(args.val)
+// }
+
+const toNumber = Number;
+const roundNumber = Math.round;
+
+
+const petFn = R.compose(
+    // writeLogHi,
+    // petLog
+    R.tap(petGetError),
+    petSetValidationError,
+    R.tap(petLogConverge),
+    R.tap(petLogConverge)
+);
+// petFn(petObj);
+console.log(petFn(petObj));
+// const processSequence = (args) => args.writeLog("hi");
+
+
--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -1,3 +1,5 @@
+import * as R from "ramda";
+
 /**
  * @file Домашка по FP ч. 1
  *
@@ -22,29 +24,161 @@ export const validateFieldN1 = ({star, square, triangle, circle}) => {
     return star === 'red' && square === 'green';
 };
 
+// console.clear();
+
+// console.log("hi");
+const myObj = { star: "blue", triangle: "blue", circle: "red", square: "blue" };
+
+// Functions predicates
+const isTwo = R.equals(2);
+const isGreaterOrEqualTwo = R.lte(2);
+// console.log(isGreaterOrEqualTwo(1))
+
+const isRed = R.equals("red");
+const isGreen = R.equals("green");
+const isBlue = R.equals("blue");
+const isWhite = R.equals("white");
+const isOrange = R.equals("orange");
+
+const triangleProp = R.prop("triangle");
+const squareProp = R.prop("square");
+const circleProp = R.prop("circle");
+const starProp = R.prop("star");
+
+// const starIsRed = R.propEq("star", "red");
+// const starIsGreen = R.propEq("star", "green");
+// const starIsBlue = R.propEq("star", "blue");
+// const starIsWhite = R.propEq("star", "white");
+// const starIsOrange = R.propEq("star", "orange");
+// console.log(isRed("green"));
+const isRedStar = R.compose(isRed, starProp);
+const isBlueStar = R.compose(isBlue, starProp);
+const isWhiteStar = R.compose(isWhite, starProp);
+const isGreenStar = R.compose(isGreen, starProp);
+const isOrangeStar = R.compose(isOrange, starProp);
+
+const isWhiteTriangle = R.compose(isWhite, triangleProp);
+const isRedTriangle = R.compose(isRed, triangleProp);
+const isGreenTriangle = R.compose(isGreen, triangleProp);
+const isOrangeTriangle = R.compose(isOrange, triangleProp);
+
+const isWhiteSquare =R.compose(isWhite, squareProp);
+const isOrangeSquare = R.compose(isOrange, squareProp);
+const isGreenSquare = R.compose(isGreen, squareProp);
+
+const isGreenCircle = R.compose(isGreen, circleProp);
+
+const isBlueCircle = R.compose(isBlue, circleProp);
+const isOrangeCircle = R.compose(isOrange, circleProp);
+
+
+const twoIsGreen = R.compose(
+    isGreaterOrEqualTwo,
+    R.length,
+    R.filter(isGreen),
+    R.values
+);
+const isAllGreen = R.allPass([isGreenCircle, isGreenSquare, isGreenStar, isGreenTriangle]);
+const numberOfRed = R.compose(
+    R.length,
+    R.filter(isRed),
+    R.values
+);
+const numberOfBlue = R.compose(
+    R.length,
+    R.filter(isBlue),
+    R.values
+);
+// const numbersRedAndBlueAreEquals = R.equals(numberOfRed, numberOfBlue);
+const toSingleArr = (fns) => (val) => fns.map((fn) => fn(val));
+// const processString = toSingleArr([((a)=>a+1), ((a)=>a+"!")]);
+const countRedAndBlue = toSingleArr([numberOfRed, numberOfBlue]);
+const allEqual = arr => arr.every(val => val === arr[0]);
+const numberOfRedIsEqualsNumberOfBlue = R.compose(
+    allEqual,
+    countRedAndBlue
+)
+// console.log(toSingleArr([((a)=>a+1), ((a)=>a+"!")])("hi"));
+// console.log(processString("hi"));
+// console.log(countRedAndBlue(myObj));
+// console.log(numberOfRedIsEqualsNumberOfBlue(myObj));
+// const processHi = processString("hi");
+// console.log(R.ap([numberOfBlue, numberOfRed], [myObj]));
+
+
+// console.log(starIsRed(myObj));
+// console.log(isRedStarProp(myObj))
+// console.log(isAllGreen(myObj))
+// console.log(twoIsGreen(myObj))
+
+const blueCircleRedStarOrangeSquare = R.allPass([isBlueCircle, isRedStar, isOrangeSquare]);
+
+const notEqualsWhite = R.compose(
+    R.not,
+    R.equals("white")
+);
+
+const hasThreeCommonColor = R.compose(
+    R.lte(3),
+    R.nth(0),
+    R.values,
+    R.countBy(R.toLower),
+    R.filter(notEqualsWhite),
+    R.values
+);
+
+const hasTwoGreen = R.compose(
+    R.lte(2),
+    R.length,
+    R.filter(isGreen),
+    R.values
+);
+
+const hasAnyRed = R.compose(
+    R.find(isRed),
+    R.values
+);
+
+const hasGreenTriangleGreenAnyRedAny = R.allPass([isGreenTriangle, hasTwoGreen, hasAnyRed]);
+
+// console.log(hasGreenTriangleGreenAnyRedAny(myObj));
+
+const isAllOrange = R.allPass([isOrangeCircle, isOrangeSquare, isOrangeStar, isOrangeTriangle]);
+
+const isNotRedStar = R.compose(R.not, isRedStar);
+const isNotWhiteStar = R.compose(R.not, isWhiteStar);
+const hasNotWhiteStarNotRedStar = R.allPass([isNotRedStar, isNotWhiteStar]);
+// console.log(isRedStar(myObj));
+
+const isNotWhiteSquare = R.compose(R.not, isWhiteSquare);
+const isNotWhiteTriangle = R.compose(R.not, isWhiteTriangle);
+
+const hasEqualSquareAndTriangle = R.converge(R.equals, [squareProp, triangleProp]);
+const hasEqualSquareAndTriangleButNotWhite = R.allPass([hasEqualSquareAndTriangle, isNotWhiteSquare, isNotWhiteTriangle]);
+
 // 2. Как минимум две фигуры зеленые.
-export const validateFieldN2 = () => false;
+export const validateFieldN2 = twoIsGreen;
 
 // 3. Количество красных фигур равно кол-ву синих.
-export const validateFieldN3 = () => false;
+export const validateFieldN3 = numberOfRedIsEqualsNumberOfBlue;
 
 // 4. Синий круг, красная звезда, оранжевый квадрат треугольник любого цвета
-export const validateFieldN4 = () => false;
+export const validateFieldN4 = blueCircleRedStarOrangeSquare;
 
 // 5. Три фигуры одного любого цвета кроме белого (четыре фигуры одного цвета – это тоже true).
-export const validateFieldN5 = () => false;
+export const validateFieldN5 = hasThreeCommonColor;
 
 // 6. Ровно две зеленые фигуры (одна из зелёных – это треугольник), плюс одна красная. Четвёртая оставшаяся любого доступного цвета, но не нарушающая первые два условия
-export const validateFieldN6 = () => false;
+export const validateFieldN6 = hasGreenTriangleGreenAnyRedAny;
 
 // 7. Все фигуры оранжевые.
-export const validateFieldN7 = () => false;
+export const validateFieldN7 = isAllOrange;
 
 // 8. Не красная и не белая звезда, остальные – любого цвета.
-export const validateFieldN8 = () => false;
+export const validateFieldN8 = hasNotWhiteStarNotRedStar;
 
 // 9. Все фигуры зеленые.
-export const validateFieldN9 = () => false;
+export const validateFieldN9 = isAllGreen;
 
 // 10. Треугольник и квадрат одного цвета (не белого), остальные – любого цвета
-export const validateFieldN10 = () => false;
+export const validateFieldN10 = hasEqualSquareAndTriangleButNotWhite;
